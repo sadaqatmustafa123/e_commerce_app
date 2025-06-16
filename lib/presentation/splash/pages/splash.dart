d315705486b3e0ecd873d9fd1253cfc49f3a117e
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-16 20:13:29 +0500

    made splash screen

---
+++ lib/presentation/splash/pages/splash.dart
@@ -0,0 +1,16 @@
+import 'package:e_commerce_app/core/configs/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.primary,
+        body: Center(
+          child: SvgPicture.asset(AppVector),
+        ));
+  }
+}
