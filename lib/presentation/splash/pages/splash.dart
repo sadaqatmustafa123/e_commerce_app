01f0fdbf485438dccee3c8ab76043f053b9d045b
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-17 13:50:47 +0500

    assets added

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,17 +1,18 @@
+import 'package:e_commerce_app/core/configs/app_vectors.dart';
 import 'package:e_commerce_app/core/configs/themes/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.primary,
       body: Center(
-        child: Icon(
-          Icons.home_filled,
-          size: 50,
+        child: SvgPicture.asset(
+          AppVectors.appLogo,
         ),
       ),
     );
