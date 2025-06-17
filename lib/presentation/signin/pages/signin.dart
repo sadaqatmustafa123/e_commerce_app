af5e30d7a48ee9c8536a9a14525a830514f360e5
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-17 14:16:21 +0500

    making sign in page

--- lib/presentation/signin/pages/signin.dart
+++ lib/presentation/signin/pages/signin.dart
@@ -5,6 +5,37 @@ class SigninPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 90,
+        ),
+        child: Column(
+          children: [
+            _signinText(
+              context,
+            ),
+            _emailField(
+              context,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signinText(BuildContext context) {
+    return const Text(
+      "Sign in",
+      style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField();
   }
 }
