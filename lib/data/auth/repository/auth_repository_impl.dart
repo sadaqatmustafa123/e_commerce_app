577a36202ec25ee4d6bb36f0b271887b9c4a2253
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-18 18:30:35 +0500

    changes in SL

--- lib/data/auth/repository/auth_repository_impl.dart
+++ lib/data/auth/repository/auth_repository_impl.dart
@@ -1,11 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce_app/data/auth/models/user_creation_req.dart';
+import 'package:e_commerce_app/data/auth/source/auth_firebase_service.dart';
 import 'package:e_commerce_app/domain/auth/repository/auth_repository.dart';
+import 'package:e_commerce_app/service_locator.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
   @override
-  Future<Either> signUp(UserCreationReq user) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<Either> signUp(UserCreationReq user) async {
+    return sl<AuthFirebaseService>().signUp(user);
   }
 }
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,10 +1,27 @@
+import 'package:e_commerce_app/core/usecase/usecase.dart';
+import 'package:e_commerce_app/data/auth/repository/auth_repository_impl.dart';
+import 'package:e_commerce_app/data/auth/source/auth_firebase_service.dart';
+import 'package:e_commerce_app/domain/auth/repository/auth_repository.dart';
+import 'package:e_commerce_app/domain/usecases/signup_usecase.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 Future<void> initializeDependencies() async {
   // Services
 
+  sl.registerSingleton<AuthFirebaseService>(
+    AuthFirebaseServiceImpl(),
+  );
+
   // Repositories
 
+  sl.registerSingleton<AuthRepository>(
+    AuthRepositoryImpl(),
+  );
+
   // Usecases
+
+  sl.registerSingleton<SignupUsecase>(
+    SignupUsecase(),
+  );
 }
