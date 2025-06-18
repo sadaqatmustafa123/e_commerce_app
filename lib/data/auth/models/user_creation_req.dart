2f23be436861434cbd6825285ae2f1de6a6283a2
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-18 16:16:52 +0500

    add user model and implementation in abstract class

--- lib/data/auth/models/user_creation_req.dart
+++ lib/data/auth/models/user_creation_req.dart
@@ -1,1 +1,13 @@
+class UserCreationReq {
+  String? firstName;
+  String? lastName;
+  String? password;
+  String? email;
 
+  UserCreationReq({
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.password,
+  });
+}
--- lib/domain/auth/repository/auth_repository.dart
+++ lib/domain/auth/repository/auth_repository.dart
@@ -1,5 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/data/auth/models/user_creation_req.dart';
 
 abstract class AuthRepository {
-  Future<Either> signUp();
+  Future<Either> signUp(
+    UserCreationReq user,
+  );
 }
