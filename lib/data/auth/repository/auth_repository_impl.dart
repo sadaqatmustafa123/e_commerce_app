85d9dbda5ccf0f2ded8e5ddebc28ab1ce6e560bc
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-25 21:09:30 +0500

    changes

--- lib/data/auth/repository/auth_repository_impl.dart
+++ lib/data/auth/repository/auth_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce_app/data/auth/models/user_creation_req.dart';
+import 'package:e_commerce_app/data/auth/models/user_signin_req.dart';
 import 'package:e_commerce_app/data/auth/source/auth_firebase_service.dart';
 import 'package:e_commerce_app/domain/auth/repository/auth_repository.dart';
 import 'package:e_commerce_app/service_locator.dart';
@@ -13,5 +14,10 @@ class AuthRepositoryImpl extends AuthRepository {
   @override
   Future<Either> getAges() async {
     return await sl<AuthFirebaseService>().getAges();
+  }
+
+  @override
+  Future<Either> signIn(UserSigninReq user) async {
+    return await sl<AuthFirebaseService>().signIn(user);
   }
 }
--- lib/domain/auth/repository/auth_repository.dart
+++ lib/domain/auth/repository/auth_repository.dart
@@ -1,12 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce_app/data/auth/models/user_creation_req.dart';
+import 'package:e_commerce_app/data/auth/models/user_signin_req.dart';
 
 abstract class AuthRepository {
   Future<Either> signUp(
     UserCreationReq user,
   );
   Future<Either> signIn(
-    UserCreationReq user,
+    UserSigninReq user,
   );
   Future<Either> getAges();
 }
