8a9afc1bdf2efcc2896521f0a69f76915f28df9a
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-30 14:22:43 +0500

    add: sign in use case

---
+++ lib/domain/usecases/signin_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/core/usecase/usecase.dart';
+import 'package:e_commerce_app/data/auth/models/user_creation_req.dart';
+import 'package:e_commerce_app/domain/auth/repository/auth_repository.dart';
+
+import '../../service_locator.dart';
+
+class SignupUsecase implements Usecase<Either, UserCreationReq> {
+  @override
+  Future<Either> call({UserCreationReq? params}) async {
+    return await sl<AuthRepository>().signUp(params!);
+  }
+}
