3a37cc841e942028e5197be0684cbd991f8cfcce
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-30 16:55:34 +0500

    change: changes in password screen

--- lib/presentation/auth/pages/enter_password.dart
+++ lib/presentation/auth/pages/enter_password.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/common/bloc/button/button_state_cubit.dart';
 import 'package:e_commerce_app/common/helper/navigator/app_navigator.dart';
 import 'package:e_commerce_app/common/widgets/appbar/app_bar.dart';
 import 'package:e_commerce_app/common/widgets/button/basic_app_button.dart';
@@ -5,6 +6,9 @@ import 'package:e_commerce_app/data/auth/models/user_signin_req.dart';
 import 'package:e_commerce_app/presentation/auth/pages/forgot_password.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../common/bloc/button/button_state.dart';
 
 class EnterPasswordPage extends StatelessWidget {
   final UserSigninReq userSigninReq;
@@ -16,36 +20,57 @@ class EnterPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BasicAppbar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 00,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _signinText(
-              context,
+        appBar: const BasicAppbar(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 00,
+          ),
+          child: BlocProvider(
+            create: (context) => ButtonStateCubit(),
+            child: BlocListener<ButtonStateCubit, ButtonState>(
+              listener: (context, state) {
+                if (state is ButtonFailureState) {
+                  var snackbar = SnackBar(
+                    content: Text(
+                      state.errorMessage,
+                    ),
+                    behavior: SnackBarBehavior.floating,
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                }
+                if (state is ButtonSuccessState) {
+                  var successSnackbar = const SnackBar(
+                    content: Text("Sign up successful"),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(successSnackbar);
+                }
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _signinText(
+                    context,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  _passwordField(
+                    context,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  _continueButton(context),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  _forgotPassword(context),
+                ],
+              ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            _passwordField(
-              context,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            _continueButton(context),
-            const SizedBox(
-              height: 20,
-            ),
-            _forgotPassword(context),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 
   Widget _signinText(BuildContext context) {
