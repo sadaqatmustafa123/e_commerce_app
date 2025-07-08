e21aa6a665af49ad1b1abfcd513e474e1c62490a
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-07-08 18:03:58 +0500

    fix: changes

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/data/auth/source/auth_firebase_service.dart';
 import 'package:e_commerce_app/domain/usecases/get_user_usecase.dart';
 import 'package:e_commerce_app/presentation/home/widgets/header.dart';
 import 'package:flutter/material.dart';
@@ -7,14 +8,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(
             horizontal: 10,
           ),
           child: Column(
-            children: [Header(), Text("$GetUserUsecase")],
+            children: [
+              Header(),
+            ],
           ),
         ),
       ),
