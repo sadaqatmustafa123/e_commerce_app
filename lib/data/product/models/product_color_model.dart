73361e7bdf5ddb6667568690197f4c425604f199
Sadaqat Mustafa <sadaqatmustafa12345@gmail.com>
2025-07-21 12:48:20 +0500

    fix: changes in color model of product

--- lib/data/product/models/product_color_model.dart
+++ lib/data/product/models/product_color_model.dart
@@ -1,9 +1,37 @@
 class ProductColorModel {
   final String title;
-  final String hexCode;
+  final List<int> rgb;
 
   ProductColorModel({
     required this.title,
-    required this.hexCode,
+    required this.rgb,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'rgb': rgb,
+    };
+  }
+
+  factory ProductColorModel.fromMap(Map<String, dynamic> map) {
+    return ProductColorModel(
+      title: map['title'] as String,
+      rgb: List<int>.from(
+        map['rgb'].map((e) => e),
+      ),
+    );
+  }
+}
+
+extension ProductColorXModel on ProductColorModel {
+  ProductColorEntity toEntity() {
+    return ProductColorEntity(title: title, rgb: rgb);
+  }
+}
+
+extension ProductColorXEntity on ProductColorEntity {
+  ProductColorModel fromEntity() {
+    return ProductColorModel(title: title, rgb: rgb);
+  }
 }
