c012fd1d85f7fa0129a74b369645bee200266474
Sadaqat Mustafa <sadaqatmustafa12345@gmail.com>
2025-07-28 12:36:14 +0500

    fix: working on product repo impl

---
+++ lib/data/product/repositories/product_repository_impl.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/domain/products/repositories/product_repository.dart';
+
+class ProductRepositoryImpl extends ProductRepository {
+  @override
+  Future<Either> getTopSelling() async {}
+}
