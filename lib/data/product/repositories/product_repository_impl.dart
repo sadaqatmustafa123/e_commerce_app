d0abc99b80cc55f69d7df35610aac9129b83719c
Sadaqat Mustafa <sadaqatmustafa12345@gmail.com>
2025-07-28 12:56:13 +0500

    fix: changes

--- lib/data/product/repositories/product_repository_impl.dart
+++ lib/data/product/repositories/product_repository_impl.dart
@@ -1,7 +1,19 @@
 import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/data/product/source/product_firebase_service.dart';
 import 'package:e_commerce_app/domain/products/repositories/product_repository.dart';
+
+import '../../../service_locator.dart';
+import '../models/product_model.dart';
 
 class ProductRepositoryImpl extends ProductRepository {
   @override
-  Future<Either> getTopSelling() async {}
+  Future<Either> getTopSelling() async {
+    var returnedData = await sl<ProductFirebaseService>().getTopSelling();
+    return returnedData.fold((error) {
+      return Left(error);
+    }, (data) {
+      return Right(
+          List.from(data).map((e) => ProductModel.fromMap(e).toEntity()));
+    });
+  }
 }
